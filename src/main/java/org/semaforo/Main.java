86c224fda149ad422e9bc1d532e8e8c2d0914e62
Juan Zamudio <70230254+jdzamudio5@users.noreply.github.com>
2024-05-10 17:31:49 -0500

    actualizar main

---
+++ src/main/java/org/semaforo/Main.java
@@ -0,0 +1,17 @@
+package org.semaforo;
+
+public class Main {
+    public static void main(String[] args) {
+        String semaforo;
+        semaforo="AmaRillO";
+        if (semaforo.toLowerCase().equals("verde")){
+            System.out.println("puede pasar");
+        } else if (semaforo.toLowerCase().equals("amarillo")) {
+            System.out.println("preparese para detenerse");
+        } else if (semaforo.toLowerCase().equals("rojo")) {
+            System.out.println("Deténgase");
+        }else {
+            System.out.println("Error, vuelva a intentarlo");
+        }
+    }
+}
